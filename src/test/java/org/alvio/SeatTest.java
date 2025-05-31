42991248f6349f6904e3aa2604636636ee98cad8
sd12key <166713967+sd12key@users.noreply.github.com>
2025-05-31 16:51:58 -0230

    Added Seat class with tests

---
+++ src/test/java/org/alvio/SeatTest.java
@@ -0,0 +1,29 @@
+package org.alvio;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SeatTest {
+
+    @Test
+    void newSeatShouldNotBeReserved() {
+        Seat seat = new Seat(33, 1);
+        assertFalse(seat.isReserved());
+    }
+
+    @Test
+    void reserveShouldMarkSeatAsReserved() {
+        Seat seat = new Seat(7, 55);
+        seat.reserve();
+        assertTrue(seat.isReserved());
+    }
+
+    @Test
+    void cancelReservationShouldUnmarkSeat() {
+        Seat seat = new Seat(3, 77);
+        seat.reserve();
+        seat.cancelReservation();
+        assertFalse(seat.isReserved());
+    }
+
+}
